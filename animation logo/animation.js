e4e8c0f69984eb1da8a6f95e3b388563a8231321
Lê Toàn <lethanhtoan1122@gmail.com>
2022-03-24 11:38:10 +0700

    sd

--- animation logo/animation.js
+++
@@ -1,6 +0,0 @@
-const text= document.querySelector('.text p');
-text.innerHTML=text.innerText.split("").map(
-    (char, i) =>
-    `<span style="transform:rotate(${i*13.2}deg)">${char}</span>`
-
-).join("") 
